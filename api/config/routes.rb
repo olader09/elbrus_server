b6bdf4cd1074e2e0bca674262ebacaada0c7d842
Igor <reid09@mail.ru>
2020-10-25 03:43:21 +0300

    fix bug

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,12 +7,11 @@ Rails.application.routes.draw do
   post :user_token, to: 'user_token#create'
   resource :user do
     put 'join_to_event/:id', to: 'users#join_to_event', on: :member
-  
   end
 
   post :superuser_token, to: 'superuser_token#create'
   resource :superuser do
-    get 'show_user/:id', to: 'superusers#show_user', on: :members
+    get 'show_user/:id', to: 'superusers#show_user', on: :member
     put 'update_user/:id', to: 'superusers#update_user', on: :member
     get :index_users, on: :member
   end
